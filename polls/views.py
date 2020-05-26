b45317f143aa50757aa5e94ac9a8674738c2ea7d
Oscar Newman <oscar@Oscars-MacBook-Pro.local>
2020-05-26 14:22:27 -0400

    add models to admin and stub routes

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 
 # Create your views here.
 
+
 def index(request):
     return HttpResponse("Hello World. This is the polls index.")
+
+
+def detail(request: HttpRequest, question_id: int) -> HttpResponse:
+    return HttpResponse(f"You're looking at question {question_id}")
+
+
+def results(request: HttpRequest, question_id: int) -> HttpResponse:
+    return HttpResponse("Results page")
+
+
+def vote(request: HttpRequest, question_id: int) -> HttpResponse:
+    return HttpResponse("Vote Page")
